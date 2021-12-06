858222b6cd1ebae9fc861fb21a6ff4963fc910a9
Lies Tdj <liestidjedam@gmail.com>
2021-12-06 23:27:23 +0100

    Creation du JSON spriteMovePosition pour les 4 mouvements de droite

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,47 @@
 'use strict'
+
+const spriteMovePosition = {
+    attackRight: [{
+            // maskContainerHeight: 145,
+            maskContainerWidth: 80,
+            spritePositionLeft: 0
+            // spritePositionTop: 0
+        },
+        {
+            maskContainerWidth: 104,
+            spritePositionLeft: -223
+        },
+        {
+            maskContainerWidth: 119,
+            spritePositionLeft: -439
+        },
+        {
+            maskContainerWidth: 136,
+            spritePositionLeft: -649
+        },
+        {
+            maskContainerWidth: 133,
+            spritePositionLeft: -845
+        },
+        {
+            maskContainerWidth: 133,
+            spritePositionLeft: -1059
+        },
+        {
+            maskContainerWidth: 190,
+            spritePositionLeft: -1303
+        },
+        {
+            maskContainerWidth: 147,
+            spritePositionLeft: -1526
+        },
+        {
+            maskContainerWidth: 111,
+            spritePositionLeft: -1746
+        },
+    ],
+}
+
+$(function() {
+
+});
