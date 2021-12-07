2cbb55293b47b74ab9098efa73823cb6252c9f9e
Lies Tdj <liestidjedam@gmail.com>
2021-12-07 12:23:55 +0100

    Creation du JSON spriteMovePosition pour les 4 mouvements de gauche et en statique

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,95 @@
 'use strict'
 
 const spriteMovePosition = {
+    staticRightAndLeft: [{
+            // first static right position
+            maskContainerWidth: 80,
+            spritePositionLeft: -4529
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -4688
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -4845
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -5005
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -5163
+        },
+
+        // second static right position
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -4529
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -4688
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -4845
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -5005
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -5163
+        },
+        
+        // first static left position
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -9012
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -8855
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -8700
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -8544
+        },
+        {
+            maskContainerWidth: 80,
+                spritePositionLeft: -8389
+        },
+
+        // second static left position
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -9012
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -8855
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -8700
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -8544
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -8389
+        },
+    ],
+    
     attackRight: [{
             // maskContainerHeight: 145,
             maskContainerWidth: 80,
@@ -8,8 +97,8 @@ const spriteMovePosition = {
             // spritePositionTop: 0
         },
         {
-            maskContainerWidth: 104,
-            spritePositionLeft: -223
+        maskContainerWidth: 104,
+        spritePositionLeft: -223
         },
         {
             maskContainerWidth: 119,
@@ -40,7 +129,215 @@ const spriteMovePosition = {
             spritePositionLeft: -1746
         },
     ],
+
+    deathRight: [{
+            // maskContainerHeight: 145,
+            maskContainerWidth: 83,
+            spritePositionLeft: -2197
+            // spritePositionTop: 0
+        },
+        {
+            maskContainerWidth: 106,
+            spritePositionLeft: -2367
+        },
+        {
+            maskContainerWidth: 129,
+            spritePositionLeft: -2522
+        },
+        {
+            maskContainerWidth: 155,
+            spritePositionLeft: -2671
+        },
+        {
+            maskContainerWidth: 159,
+            spritePositionLeft: -2827
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -2988
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -3151
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -3312
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -3474
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -3635
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -3797
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -3959
+        },
+    ],
+
+
+    runRight: [{
+            maskContainerWidth: 80,
+            spritePositionLeft: -5660
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -5818
+        },
+        {
+            maskContainerWidth: 84,
+            spritePositionLeft: -5973
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -6135
+        },
+        {
+            maskContainerWidth: 82,
+            spritePositionLeft: -6292
+        },
+        {
+            maskContainerWidth: 82,
+            spritePositionLeft: -6451
+        },
+        {
+            maskContainerWidth: 82,
+            spritePositionLeft: -6610
+        },
+    ],
+    
+    attackLeft: [{
+            // maskContainerHeight: 145,
+            maskContainerWidth: 80,
+            spritePositionLeft: -13460
+            // spritePositionTop: 0
+        },
+        {
+            maskContainerWidth: 106,
+            spritePositionLeft: -13217
+        },
+        {
+            maskContainerWidth: 120,
+            spritePositionLeft: -12990
+        },
+        {
+            maskContainerWidth: 136,
+            spritePositionLeft: -12768
+        },
+        {
+            maskContainerWidth: 132,
+            spritePositionLeft: -12578
+        },
+        {
+            maskContainerWidth: 132,
+            spritePositionLeft: -12368
+        },
+        {
+            maskContainerWidth: 190,
+            spritePositionLeft: -12074
+        },
+        {
+            maskContainerWidth: 147,
+            spritePositionLeft: -11896
+        },
+        {
+            maskContainerWidth: 111,
+            spritePositionLeft: -11714
+        },
+    ],
+
+    deathLeft: [{
+            // maskContainerHeight: 145,
+            maskContainerWidth: 83,
+            spritePositionLeft: -11299
+            // spritePositionTop: 0
+        },
+        {
+            maskContainerWidth: 106,
+            spritePositionLeft: -11109
+        },
+        {
+            maskContainerWidth: 129,
+            spritePositionLeft: -10935
+        },
+        {
+            maskContainerWidth: 155,
+            spritePositionLeft: -10764
+        },
+        {
+            maskContainerWidth: 159,
+            spritePositionLeft: -10606
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -10464
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -10304
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -10146
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -9986
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -9827
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -9668
+        },
+        {
+            maskContainerWidth: 143,
+            spritePositionLeft: -9509
+        },
+    ],
+
+
+    runLeft: [{
+            maskContainerWidth: 80,
+            spritePositionLeft: -7901
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -7745
+        },
+        {
+            maskContainerWidth: 84,
+            spritePositionLeft: -7589
+        },
+        {
+            maskContainerWidth: 80,
+            spritePositionLeft: -7434
+        },
+        {
+            maskContainerWidth: 82,
+            spritePositionLeft: -7278
+        },
+        {
+            maskContainerWidth: 82,
+            spritePositionLeft: -7123
+        },
+        {
+            maskContainerWidth: 82,
+            spritePositionLeft: -6968
+        },
+    ]
 }
+
+
 
 $(function() {
 
